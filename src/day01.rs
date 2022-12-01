d9b455fd167fc510a4dc56dbd344f9f4e84eff67
tpyd <112725276+tpyd@users.noreply.github.com>
2022-12-01 17:46:12 +0100

    Finished day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,36 @@
 pub fn part1(input: String) {
-    println!("{}", input.chars().rev().collect::<String>());
+    let calories: u32 = input.split("\n\n")
+        .map(|elf| {
+            elf
+                .trim()
+                .split('\n')
+                .map(|x| x.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .max()
+        .unwrap();
+
+    println!("The elf with the most calories has {:?} calories", calories);
+}
+
+pub fn part2(input: String) {
+    let mut elves: Vec<u32> = input.split("\n\n")
+        .map(|elf| {
+            elf
+                .trim()
+                .split('\n')
+                .map(|x| x.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .collect();
+
+    elves.sort_unstable();
+
+    let calories: u32 = elves
+        .into_iter()
+        .rev()
+        .take(3)
+        .sum();
+
+    println!("The top 3 elves has a combined {:?} calories", calories);
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 use std::io;
 use std::time::{Duration, Instant};
 
-use advent_of_code::{get_day, noop};
+use aoc::{get_day, noop};
 
 fn fmt_time(ms: f64) -> String {
     if ms <= 1.0 {
