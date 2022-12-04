1e0c653ddcb20e5b7f0bdd5d404e0c25de20323a
tpyd <112725276+tpyd@users.noreply.github.com>
2022-12-04 10:57:25 +0100

    Day 4 optimizations

--- src/day04.rs
+++ src/day04.rs
@@ -5,25 +5,14 @@ pub fn part1(input: String) {
         .trim()
         .split('\n')
         .map(|pair| {
-            let assignments = pair
-                .split(',')
-                .map(|assignment| {
-                    assignment
-                        .split('-')
-                        .map(|x| x.parse::<u32>().unwrap())
-                        .collect_tuple::<(u32, u32)>().unwrap()
-                })
-                .collect_tuple::<((u32, u32), (u32, u32))>().unwrap();
+            let assignments: (u32, u32, u32, u32) = pair
+                .split(|x| !char::is_numeric(x))
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect_tuple().unwrap();
 
-            let ((a1, a2), (b1, b2)) = assignments;
+            let (a1, a2, b1, b2) = assignments;
 
-            // a contains b
-            if a1 <= b1 && a2 >= b2 {
-                return true
-            }
-
-            // b contains a
-            if b1 <= a1 && b2 >= a2 {
+            if (a1 <= b1 && a2 >= b2) || (b1 <= a1 && b2 >= a2) {
                 return true
             }
 
@@ -40,17 +29,12 @@ pub fn part2(input: String) {
         .trim()
         .split('\n')
         .map(|pair| {
-            let assignments = pair
-                .split(',')
-                .map(|assignment| {
-                    assignment
-                        .split('-')
-                        .map(|x| x.parse::<u32>().unwrap())
-                        .collect_tuple::<(u32, u32)>().unwrap()
-                })
-                .collect_tuple::<((u32, u32), (u32, u32))>().unwrap();
+            let assignments: (u32, u32, u32, u32) = pair
+                .split(|x| !char::is_numeric(x))
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect_tuple().unwrap();
 
-            let ((a1, a2), (b1, b2)) = assignments;
+            let (a1, a2, b1, b2) = assignments;
 
             if a1 > b2 || b1 > a2 {
                 return false
