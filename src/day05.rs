69d98d9933d40db0129a0d793065f26232f036bf
tpyd <112725276+tpyd@users.noreply.github.com>
2022-12-06 20:13:23 +0100

    Finished day 5 part 1

--- src/day05.rs
+++ src/day05.rs
@@ -1,24 +1,47 @@
 use itertools::Itertools;
 
 pub fn part1(input: String) {
-    let (crates, moves) = input
+    let (_, moves) = input
         .split("\n\n")
         .map(str::trim)
         .collect_tuple().unwrap();
 
-    let num_stacks = crates
+    let mut stacks = vec![
+        vec!['B', 'L', 'D', 'T', 'W', 'C', 'F', 'M'],
+        vec!['N', 'B', 'L'],
+        vec!['J', 'C', 'H', 'T', 'L', 'V'],
+        vec!['S', 'P', 'J', 'W'],
+        vec!['Z', 'S', 'C', 'F', 'T', 'L', 'R'],
+        vec!['W', 'D', 'G', 'B', 'H', 'N', 'Z'],
+        vec!['F', 'M', 'S', 'P', 'V', 'G', 'C', 'N'],
+        vec!['W', 'Q', 'R', 'J', 'F', 'V', 'C', 'Z'],
+        vec!['R', 'P', 'M', 'L', 'H']
+    ];
+
+    let instructions: Vec<(usize, usize, usize)> = moves
         .split('\n')
-        .next_back().unwrap()
-        .split(|x| !char::is_numeric(x))
-        .map(|x| {println!("{:?}", x); 1})
-        // .map(|x| x.parse::<u32>().unwrap())
-        .max().unwrap();
+        .map(|line| {
+            line
+                .split(|x: char| !x.is_numeric())
+                .filter(|x| !x.is_empty())
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect_tuple::<(usize, usize, usize)>().unwrap()
+        })
+        .collect();
 
+    for (num, from, to) in instructions {
+        for _ in 0..num {
+            let ele = stacks[from-1].pop().unwrap();
+            stacks[to-1].push(ele);
+        }
+    }
 
-    println!("{:?}", num_stacks);
+    let top_crates = stacks
+        .iter()
+        .map(|x| x.last().unwrap())
+        .join("");
 
-
-    println!("Part 1: {:?}", 0);
+    println!("Part 1: {:?}", top_crates);
 }
 
 pub fn part2(input: String) {
