5b6d64fa88f55654d40fd94ac980179ec6b18cff
tpyd <112725276+tpyd@users.noreply.github.com>
2022-12-06 20:43:35 +0100

    Finished day 5

--- src/day05.rs
+++ src/day05.rs
@@ -45,7 +45,44 @@ pub fn part1(input: String) {
 }
 
 pub fn part2(input: String) {
-    let sum = 0;
+    let (_, moves) = input
+        .split("\n\n")
+        .map(str::trim)
+        .collect_tuple().unwrap();
 
-    println!("Part 2: {:?}", sum);
+    let mut stacks = vec![
+        vec!['B', 'L', 'D', 'T', 'W', 'C', 'F', 'M'],
+        vec!['N', 'B', 'L'],
+        vec!['J', 'C', 'H', 'T', 'L', 'V'],
+        vec!['S', 'P', 'J', 'W'],
+        vec!['Z', 'S', 'C', 'F', 'T', 'L', 'R'],
+        vec!['W', 'D', 'G', 'B', 'H', 'N', 'Z'],
+        vec!['F', 'M', 'S', 'P', 'V', 'G', 'C', 'N'],
+        vec!['W', 'Q', 'R', 'J', 'F', 'V', 'C', 'Z'],
+        vec!['R', 'P', 'M', 'L', 'H']
+    ];
+
+    let instructions: Vec<(usize, usize, usize)> = moves
+        .split('\n')
+        .map(|line| {
+            line
+                .split(|x: char| !x.is_numeric())
+                .filter(|x| !x.is_empty())
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect_tuple::<(usize, usize, usize)>().unwrap()
+        })
+        .collect();
+
+    for (num, from, to) in instructions {
+        let len = stacks[from-1].len();
+        let ele = stacks[from-1].split_off(len - num);
+        stacks[to-1].extend(ele);
+    }
+
+    let top_crates = stacks
+        .iter()
+        .map(|x| x.last().unwrap())
+        .join("");
+
+    println!("Part 2: {:?}", top_crates);
 }
