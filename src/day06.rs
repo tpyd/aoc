cddbd02d8b69a0892e922b5c679dd37a11c15c7c
tpyd <112725276+tpyd@users.noreply.github.com>
2022-12-08 18:41:53 +0100

    Finished day 6

--- src/day06.rs
+++ src/day06.rs
@@ -26,23 +26,32 @@ pub fn part1(input: String) {
 
 pub fn part2(input: String) {
     const num_unique: usize = 14;
+
     let mut i = 0;
 
     loop {
-        let substr = &input[i..i+4];
+        let substr = &input[i..i+num_unique];
+        let mut valid = true;
 
-        let (a, b, c, d) = substr
-            .as_bytes()
-            .into_iter()
-            .collect_tuple()
-            .unwrap();
+        // Test each character
+        for (n1, c1) in substr.chars().enumerate() {
+            // Loop again over all characters
+            for (n2, c2) in substr.chars().enumerate() {
+                // Characters are same and different position
+                if c1 == c2 && n1 != n2 {
+                    valid = false;
+                }
+            }
+        }
 
-
+        if valid {
+            break
+        }
 
         i += 1;
     }
 
-    let num = i + 4;
+    let num = i + num_unique;
 
     println!("Part 2: {:?}", num);
 }
