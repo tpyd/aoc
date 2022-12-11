1680cf36859a218b7d5ed485497a4b7ebe2fe1de
tpyd <112725276+tpyd@users.noreply.github.com>
2022-12-11 10:43:45 +0100

    Finished day 9

--- src/day09.rs
+++ src/day09.rs
@@ -60,6 +60,57 @@ pub fn part1(input: String) {
 }
 
 pub fn part2(input: String) {
+    let instructions = input
+        .trim()
+        .split('\n')
+        .map(|x| {
+            let (d, n) = x
+                .split(' ')
+                .collect_tuple().unwrap();
 
-    println!("Part 2: {:?}", 0);
+            let m = str::parse::<u32>(n).unwrap();
+            (d, m)
+        })
+        .collect::<Vec<(&str, u32)>>();
+
+    let num_ropes = 10;
+    let mut ropes: Vec<Point> = Vec::new();
+    for _ in 0..num_ropes {
+        ropes.push(Point{x: 0, y: 0});
+    }
+
+    let mut tail_pos: HashSet<Point> = HashSet::new();
+
+    for (dir, n) in instructions {
+        for _ in 0..n {
+            let mut head = &mut ropes[0];
+            match dir {
+                "R" => head.x += 1,
+                "L" => head.x -= 1,
+                "U" => head.y += 1,
+                "D" => head.y -= 1,
+                _ => panic!(),
+            }
+
+            // Update ropes
+            for i in 0..num_ropes-1 {
+                let a = ropes[i];
+                let mut b = &mut ropes[i+1];
+
+                let (xdiff, ydiff) = distance(a, *b);
+
+                // Move rope
+                if xdiff + ydiff == 3 || xdiff == 2 || ydiff == 2  {
+                    b.x += (a.x - b.x).clamp(-1, 1);
+                    b.y += (a.y - b.y).clamp(-1, 1);
+                }
+            }
+
+            tail_pos.insert(*ropes.last().unwrap());
+        }
+    }
+
+    let num_tail_pos = tail_pos.len();
+
+    println!("Part 2: {:?}", num_tail_pos);
 }
