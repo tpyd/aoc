b75efddc22a69fdfb1bd9b6d3c5e9ef2bcd0f546
tpyd <112725276+tpyd@users.noreply.github.com>
2023-12-08 21:18:50 +0100

    Finished day 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,16 +1,4 @@
 use itertools::Itertools;
-use nom::{
-    IResult,
-    bytes::complete::{tag, take_while_m_n},
-    combinator::map_res,
-    sequence::{tuple, terminated}, character::complete::{char, digit1}, multi::separated_list1, branch::alt};
-
-#[derive(Debug, Clone, Copy)]
-enum Color {
-    Red,
-    Green,
-    Blue
-}
 
 #[derive(Debug, Clone, Copy)]
 struct Reveal {
@@ -25,74 +13,15 @@ struct Game {
     reveals: Vec<Reveal>
 }
 
-impl Reveal {
-    fn new() -> Self {
-        Reveal { red: 0, green: 0, blue: 0 }
-    }
-}
-
-fn parse_color(input: &str) -> IResult<&str, Color> {
-    let (input, color) = alt((tag("red"), tag("green"), tag("blue")))(input)?;
-    match color {
-        "red" => Ok(("", Color::Red)),
-        "green" => Ok(("", Color::Green)),
-        "blue" => Ok(("", Color::Blue)),
-        _ => panic!()
-    }
-}
-
-fn parse_num(input: &str) -> IResult<&str, (u32, Color)> {
-    let (input, number) = map_res(digit1, |s: &str| s.parse::<u32>())(input)?;
-    let (input, _) = tag(" ")(input)?;
-    let (input, color) = parse_color(input)?;
-
-    Ok((input, (number, color)))
-}
-
-fn parse_reveal(input: &str) -> IResult<&str, Reveal> {
-    let (input, color_reveals) = separated_list1(tag(", "), parse_num)(input)?;
-
-    let mut reveal = Reveal::new();
-    for (num, color) in color_reveals {
-        match color {
-            Color::Red => reveal.red += num,
-            Color::Green => reveal.green += num,
-            Color::Blue => reveal.blue += num,
-        }
-    }
-
-    Ok((input, reveal))
-}
-
-fn parse_game(input: &str) -> IResult<&str, Game> {
-    let (input, _) = tag("Game ")(input)?;
-    let (input, game_id) = map_res(digit1, |s: &str| s.parse::<u32>())(input)?;
-    let (input, _) = tag(": ")(input)?;
-    let (input, reveals) = separated_list1(tag("; "), parse_reveal)(input)?;
-
-    Ok(("", Game{ id: game_id, reveals: reveals }))
-}
-
-
 fn parse_reveal_color_num(text: &str) -> u32 {
     let (num, _) = text.trim().split(' ').collect_tuple().unwrap();
     num.parse::<u32>().unwrap()
 }
 
-
-pub fn part1(input: String) {
-//     let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-// Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-// Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-// Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-// Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-
+fn parse_input(input: &str) -> Vec<Game> {
     let mut games: Vec<Game> = Vec::new();
 
     for line in input.trim().split('\n') {
-        // let game = parse_game(input).unwrap().1;
-        // games.push(game);
-
         let (gamestr, revealstr) = line.split(':').collect_tuple().unwrap();
         let (_, gamenumstr) = gamestr.split(' ').collect_tuple().unwrap();
         let game_num = gamenumstr.parse::<u32>().unwrap();
@@ -121,6 +50,17 @@ pub fn part1(input: String) {
         games.push(game);
     }
 
+    games
+}
+
+pub fn part1(input: String) {
+//     let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+// Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+// Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+// Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+// Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+    let games = parse_input(&input);
+
     let max_red = 12;
     let max_green = 13;
     let max_blue = 14;
@@ -145,5 +85,24 @@ pub fn part1(input: String) {
 }
 
 pub fn part2(input: String) {
+//     let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+// Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+// Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+// Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+// Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 
+    let games = parse_input(&input);
+
+    let mut powers = Vec::new();
+
+    for game in games {
+        let maxred = game.reveals.iter().map(|x| x.red).max().unwrap();
+        let maxgreen = game.reveals.iter().map(|x| x.green).max().unwrap();
+        let maxblue = game.reveals.iter().map(|x| x.blue).max().unwrap();
+
+        powers.push(maxred * maxgreen * maxblue);
+    }
+
+    let ans = powers.iter().sum::<u32>();
+    println!("{ans}");
 }
