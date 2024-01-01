0345e51ec987866828d87768fe374a145475106a
tpyd <112725276+tpyd@users.noreply.github.com>
2024-01-01 21:53:14 +0100

    Finished day 6 part 1

--- src/year2023/day06.rs
+++ src/year2023/day06.rs
@@ -1,3 +1,28 @@
-pub fn run(input: String) {
+use itertools::Itertools;
 
+pub fn run(input: String) {
+    let (times, records) = input.split('\n').collect_tuple().unwrap();
+    let mut times = times.split_whitespace().collect_vec();
+    let mut records = records.split_ascii_whitespace().collect_vec();
+
+    times.remove(0);
+    records.remove(0);
+
+    let times = times.iter().map(|x| x.parse::<u32>().unwrap()).collect_vec();
+    let records = records.iter().map(|x| x.parse::<u32>().unwrap()).collect_vec();
+
+    let mut unique = 1;
+
+    for (time, record) in times.iter().zip(records.iter()) {
+        let mut unique_ways = 0;
+        for speed in 0..=*time {
+            let distance = (time - speed) * speed;
+            if distance > *record {
+                unique_ways += 1;
+            }
+        }
+        unique *= unique_ways;
+    }
+
+    println!("{unique}");
 }
