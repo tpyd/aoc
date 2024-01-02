f58b9cbc74e1ecd34ebd5cc8fe066fc3d0884578
tpyd <112725276+tpyd@users.noreply.github.com>
2024-01-02 19:12:00 +0100

    Finished day 6 part 2

--- src/year2023/day06.rs
+++ src/year2023/day06.rs
@@ -1,6 +1,6 @@
 use itertools::Itertools;
 
-pub fn run(input: String) {
+fn part1(input: String) {
     let (times, records) = input.split('\n').collect_tuple().unwrap();
     let mut times = times.split_whitespace().collect_vec();
     let mut records = records.split_ascii_whitespace().collect_vec();
@@ -25,4 +25,50 @@ pub fn run(input: String) {
     }
 
     println!("{unique}");
+}
+
+fn part2(input: String) {
+    let (times, records) = input
+        .split('\n')
+        .collect_tuple()
+        .unwrap();
+    
+    let times: String = times.split(':').skip(1).collect();
+    let time = times
+        .replace(" ", "")
+        .parse::<u64>().unwrap(); 
+
+    let records: String = records.split(':').skip(1).collect();
+    let record = records
+        .replace(" ", "")
+        .parse::<u64>().unwrap();
+
+    let mut first_win = 0;
+    let mut last_win = 0;
+
+    for speed in 0..=time {
+        let distance = (time - speed) * speed;
+        if distance > record {
+            first_win = speed;
+            break;
+        }
+    }
+
+    for speed in (0..=time).rev() {
+        let distance = (time - speed) * speed;
+        if distance > record {
+            last_win = speed;
+            break;
+        }
+    }
+
+    // Off by one error somewhere i think
+    let ans = last_win - first_win + 1;
+
+    println!("{ans}");
+}
+
+pub fn run(input: String) {
+    part1(input.clone());
+    part2(input.clone());
 }
