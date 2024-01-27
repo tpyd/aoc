5f22b4fc95e42d088ae06f50759906e89941938a
tpyd <112725276+tpyd@users.noreply.github.com>
2024-01-27 12:23:49 +0100

    Changed day 6 to use quadratic formula solution.

--- src/year2023/day06.rs
+++ src/year2023/day06.rs
@@ -1,72 +1,5 @@
 use itertools::Itertools;
 
-fn part1(input: String) {
-    let (times, records) = input.split('\n').collect_tuple().unwrap();
-    let mut times = times.split_whitespace().collect_vec();
-    let mut records = records.split_ascii_whitespace().collect_vec();
-
-    times.remove(0);
-    records.remove(0);
-
-    let times = times.iter().map(|x| x.parse::<u32>().unwrap()).collect_vec();
-    let records = records.iter().map(|x| x.parse::<u32>().unwrap()).collect_vec();
-
-    let mut unique = 1;
-
-    for (time, record) in times.iter().zip(records.iter()) {
-        let mut unique_ways = 0;
-        for speed in 0..=*time {
-            let distance = (time - speed) * speed;
-            if distance > *record {
-                unique_ways += 1;
-            }
-        }
-        unique *= unique_ways;
-    }
-
-    println!("{unique}");
-}
-
-fn part2(input: String) {
-    let (times, records) = input
-        .split('\n')
-        .collect_tuple()
-        .unwrap();
-    
-    let times: String = times.split(':').skip(1).collect();
-    let time = times
-        .replace(" ", "")
-        .parse::<u64>().unwrap(); 
-
-    let records: String = records.split(':').skip(1).collect();
-    let record = records
-        .replace(" ", "")
-        .parse::<u64>().unwrap();
-
-    let mut first_win = 0;
-    let mut last_win = 0;
-
-    for speed in 0..=time {
-        let distance = (time - speed) * speed;
-        if distance > record {
-            first_win = speed;
-            break;
-        }
-    }
-
-    for speed in (0..=time).rev() {
-        let distance = (time - speed) * speed;
-        if distance > record {
-            last_win = speed;
-            break;
-        }
-    }
-
-    let ans = last_win - first_win + 1;
-
-    println!("{ans}");
-}
-
 ///
 /// Can use the quadratic formula to solve this problem. 
 /// Let t be the time you have, r the record for the race
@@ -81,12 +14,55 @@ fn part2(input: String) {
 ///       = -t +- sqrt(t^2 - 4r) / -2
 ///       = t -+ sqrt(t^2 - 4r) / 2 
 ///
-fn solve(time: f32, record: f32) -> u32 {
-    let root = f32::sqrt(time * time - 4. * record);
+fn solve(time: f64, record: f64) -> u32 {
+    let root = f64::sqrt(time * time - 4. * record);
     let lower = (((time - root) / 2.) + 1.).floor() as u32;
     let upper = ((time + root) / 2.).ceil() as u32;
 
     (lower..upper).len() as u32
+}
+
+fn part1(input: String) {
+    let (times, records) = input.split('\n').collect_tuple().unwrap();
+    
+    let mut times = times.split_whitespace().collect_vec();
+    let mut records = records.split_ascii_whitespace().collect_vec();
+
+    times.remove(0);
+    records.remove(0);
+
+    let times = times.iter().map(|x| x.parse::<f64>().unwrap()).collect_vec();
+    let records = records.iter().map(|x| x.parse::<f64>().unwrap()).collect_vec();
+
+    let mut product = 1;
+
+    for (time, record) in times.iter().zip(records.iter()) {
+        let unique_ways = solve(*time, *record);
+        product *= unique_ways;
+    }
+
+    println!("{product}");
+}
+
+fn part2(input: String) {
+    let (times, records) = input
+        .split('\n')
+        .collect_tuple()
+        .unwrap();
+    
+    let times: String = times.split(':').skip(1).collect();
+    let time = times
+        .replace(" ", "")
+        .parse::<f64>().unwrap(); 
+
+    let records: String = records.split(':').skip(1).collect();
+    let record = records
+        .replace(" ", "")
+        .parse::<f64>().unwrap();
+
+    let ans = solve(time, record);
+
+    println!("{ans}");
 }
 
 pub fn run(input: String) {
