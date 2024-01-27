0d9a3399b1fbddd4b74131ec4bf295ea60d48439
tpyd <112725276+tpyd@users.noreply.github.com>
2024-01-27 20:07:55 +0100

    Finished refactoring day 6.

--- src/year2023/day06.rs
+++ src/year2023/day06.rs
@@ -1,7 +1,8 @@
 use itertools::Itertools;
 
-///
-/// Can use the quadratic formula to solve this problem. 
+/// Gives the number of ways one can beat the record.
+/// 
+/// Solves the problem using the quadratic formula. 
 /// Let t be the time you have, r the record for the race
 /// and h the hold time for the button.
 /// 
@@ -23,42 +24,41 @@ fn solve(time: f64, record: f64) -> u32 {
 }
 
 fn part1(input: String) {
-    let (times, records) = input.split('\n').collect_tuple().unwrap();
-    
-    let mut times = times.split_whitespace().collect_vec();
-    let mut records = records.split_ascii_whitespace().collect_vec();
+    let (times, records) = input
+        .split('\n')
+        .map(|line| { 
+            line
+                .split_whitespace()
+                .skip(1)
+                .map(|x| x.parse::<f64>().unwrap())
+                .collect_vec()
+        })
+        .collect_tuple()
+        .unwrap();
 
-    times.remove(0);
-    records.remove(0);
+    let ans: u32 = times
+        .iter()
+        .zip(records.iter())
+        .map(|(t, r)| solve(*t, *r))
+        .product();
 
-    let times = times.iter().map(|x| x.parse::<f64>().unwrap()).collect_vec();
-    let records = records.iter().map(|x| x.parse::<f64>().unwrap()).collect_vec();
-
-    let mut product = 1;
-
-    for (time, record) in times.iter().zip(records.iter()) {
-        let unique_ways = solve(*time, *record);
-        product *= unique_ways;
-    }
-
-    println!("{product}");
+    println!("{ans}");
 }
 
 fn part2(input: String) {
-    let (times, records) = input
+    let (time, record) = input
         .split('\n')
+        .map(|line| {
+            line
+                .split(':')
+                .skip(1)
+                .collect::<String>()
+                .replace(" ", "")
+                .parse::<f64>()
+                .unwrap()
+        })
         .collect_tuple()
         .unwrap();
-    
-    let times: String = times.split(':').skip(1).collect();
-    let time = times
-        .replace(" ", "")
-        .parse::<f64>().unwrap(); 
-
-    let records: String = records.split(':').skip(1).collect();
-    let record = records
-        .replace(" ", "")
-        .parse::<f64>().unwrap();
 
     let ans = solve(time, record);
 
