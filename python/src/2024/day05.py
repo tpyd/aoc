32b0766121e7c243d6443cea9fff6a065f9aae66
tpyd <112725276+tpyd@users.noreply.github.com>
2024-12-05 20:37:08 +0100

    Optimized day 5

--- python/src/2024/day05.py
+++ python/src/2024/day05.py
@@ -14,20 +14,18 @@ invalid_updates = []
 
 sum = 0
 
-def is_valid(nums):
+for nums in updates:
+    valid = True
     printed = []
     for num in nums:
         nums_before = [r[1] for r in rules if r[0] == num]
         for n in nums_before:
             if n in printed:
-                return False
+                valid = False
 
         printed.append(num)
 
-    return True
-
-for nums in updates:
-    if is_valid(nums):
+    if valid:
         sum += int(nums[len(nums)//2])
     else:
         invalid_updates.append(nums)
@@ -39,15 +37,11 @@ sum = 0
 for nums in invalid_updates:
     print_order = [nums[0]]
     for num in nums[1:]:
-        put_before = [r[1] for r in rules if r[0] == num]
-        put_before = [n for n in put_before if n in nums]
-        indicies = []
-        for n in put_before:
-            if n in print_order:
-                indicies.append(print_order.index(n))
-        if indicies:
-            min_index = min(indicies)
-            print_order.insert(min_index, num)
+        insert_before = [r[1] for r in rules if r[0] == num and r[1] in print_order]
+
+        if insert_before:
+            index = min([print_order.index(n) for n in insert_before])
+            print_order.insert(index, num)
         else:
             print_order.append(num)
 
