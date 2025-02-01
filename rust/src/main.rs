6ecb87badaa760b57a7fe41139139aefc90d7656
tpyd <112725276+tpyd@users.noreply.github.com>
2025-02-01 22:31:49 +0100

    Improved running different days

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,16 +1,22 @@
+mod utils;
 mod years;
 
-use std::fs;
-
 fn main() {
-    let year = 2015;
-    let day = 1;
+    let args: Vec<String> = std::env::args().collect();
 
-    let input_path = format!("input/year{}/day{:02}.txt", year, day);
-    let input = fs::read_to_string(&input_path).expect("Could not find input file");
+    if args.len() != 3 {
+        println!("Usage: cargo run <year> <day>");
+        std::process::exit(1);
+    }
+
+    let year = args[1].parse::<usize>().expect("Year must be a number");
+    let day = args[2].parse::<usize>().expect("Day must be a number");
+
+    let input = utils::get_input(year, day);
 
     match year {
         2015 => years::year2015::run_day(day, &input),
         _ => println!("Could not find year")
     }
 }
+
---
+++ rust/src/utils.rs
@@ -0,0 +1,8 @@
+use std::fs;
+
+pub fn get_input(year: usize, day: usize) -> String {
+    let input_path = format!("input/year{}/day{:02}.txt", year, day);
+    let input = fs::read_to_string(&input_path).expect("Could not find input file");
+
+    input
+}
