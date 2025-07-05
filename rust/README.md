9c8898ad781682ce7c2cd82246da51de92b1fe40
tpyd <112725276+tpyd@users.noreply.github.com>
2025-07-05 22:24:53 +0200

    Added templates and instructions for setting up a new day.
    Cleaned up input data

--- rust/README.md
+++ rust/README.md
@@ -1,5 +1,13 @@
 # Advent of Code in Rust
 
+## Setup a new day
+- Save the input as `input/year_<year>_day_<day>.rs`
+- Copy `src/template.rs` into `src/year_<year>_day_<day>.rs` and fill in missing values
+- Go to `src/main.rs`, add the day as a module and add it to the `match` expression
+- Go to `src/lib.rs` and add it as a public module to make it visible to Criterion
+- Copy `benches/template.rs` into `benches/year_<year>_day_<day>.rs` and fill in missing values
+- Go to `Cargo.toml` and add a separate benchmark for the day 
+
 ## Solve
 `cargo run <year> <day>`
 
---
+++ rust/src/template.rs
@@ -0,0 +1,31 @@
+pub fn run(input: &str) -> (i32, i32) {
+    unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::utils;
+
+    #[test]
+    fn test_part1() {
+        let (part1, _) = run("example"); 
+        assert_eq!(part1, );
+    }    
+
+    #[test]
+    fn test_part2() {
+        let (_, part2) = run("example"); 
+        assert_eq!(part2, );
+    }
+
+    #[test]
+    fn test_real() {
+        let input = utils::read_input(, );
+        let (part1, part2) = run(&input);
+
+        assert_eq!(part1, );
+        assert_eq!(part2, );
+    }
+}
+
