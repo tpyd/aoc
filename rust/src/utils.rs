889af5ff948d037870e4bc1f7215b72582c31b9a
tpyd <112725276+tpyd@users.noreply.github.com>
2025-07-06 11:25:51 +0200

    Fixed input file path

--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 pub fn read_input(year: usize, day: usize) -> String {
-    let input_path = format!("input/year{}/day{:02}.txt", year, day);
+    let input_path = format!("input/year_{year}_day_{day:02}.txt");
     let input = fs::read_to_string(&input_path).expect("Could not find input file");
 
     input
