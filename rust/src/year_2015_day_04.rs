b15dbb1a285c81c5aa9826d2f425b3fb65fa4ae2
tpyd <112725276+tpyd@users.noreply.github.com>
2025-08-24 17:14:55 +0200

    Removed allocations when calculating md5 hash

--- rust/src/year_2015_day_04.rs
+++ rust/src/year_2015_day_04.rs
@@ -1,38 +1,16 @@
 pub fn run(input: &str) -> (u32, u32) {
-    let trimmed_input = input.trim();
-    let mut num = 0;
+    let trimmed_input = input.trim_end();
+
+    let mut num: u32 = 0;
     let mut part1 = 0;
     let mut part2 = 0;
 
-    loop {
-        let hash_input = format!("{}{}", trimmed_input, num);
-        let hash = md5(hash_input);
+    let mut a: u32;
+    let mut b: u32;
+    let mut c: u32;
+    let mut d: u32;
 
-        if hash.starts_with("00000") && part1 == 0 {
-            part1 = num;
-        }
-
-        if hash.starts_with("000000") && part2 == 0 {
-            part2 = num;
-        }
-
-        if part1 != 0 && part2 != 0 {
-            break;
-        }
-
-        num += 1;
-    }
-
-    (part1, part2)
-}
-
-fn md5(input: String) -> String {
-    let mut a: u32 = 0x67452301;
-    let mut b: u32 = 0xefcdab89;
-    let mut c: u32 = 0x98badcfe;
-    let mut d: u32 = 0x10325476;
-
-    let k: Vec<u32> = vec![
+    let k: [u32; 64] = [
          0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
          0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
          0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
@@ -51,74 +29,115 @@ fn md5(input: String) -> String {
          0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
     ];
 
-    let s: Vec<u32> = vec![
+    let s: [u32; 64] = [
         7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
         5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
         4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
         6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
     ];
 
-    let mut bytes = input.as_bytes().to_vec();
-    bytes.push(0x80);
+    let mut hash_input = [0u8; 64];
+    let mut buf = [0u8; 10];
+    let mut digits = [0u8; 10];
 
-    while bytes.len() % 64 < 56 {
-        bytes.push(0);
-    }
+    let mut offset = trimmed_input.len();
+    hash_input[0..offset].copy_from_slice(trimmed_input.as_bytes());
 
-    let original_length = input.len() as u64 * 8;
-    bytes.extend(original_length.to_le_bytes());
+    loop {
+        offset = trimmed_input.len();
 
-    for chunk in bytes.chunks(64) {
-        let mut da = a;
-        let mut db = b;
-        let mut dc = c;
-        let mut dd = d;
+        let mut n = num;
+        let mut i = 0; 
+        loop {
+            digits[i] = b'0' + (n % 10) as u8;
+            n /= 10;
+            i += 1;
 
-        let mut m = [0u32; 16];
-        for (i, m_value) in m.iter_mut().enumerate() {
-            let bytes = &chunk[i * 4 .. (i + 1) * 4];
-            *m_value = u32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]);
+            if n == 0 {
+                break;
+            }
         }
 
-        for i in 0..64 {
-            let mut f: u32;
-            let g: usize;
+        for j in 0..i {
+            buf[j] = digits[i - 1 - j];
+        }
 
-            if i <= 15 {
-                f = (db & dc) | (!db & dd);
-                g = i;
-            } else if i <= 31 {
-                f = (dd & db) | (!dd & dc); 
-                g = (5 * i + 1) % 16;
-            } else if i <= 47 {
-                f = db ^ dc ^ dd;
-                g = (3 * i + 5) % 16;
-            } else {
-                f = dc ^ (db | !dd);
-                g = (7 * i) % 16;
+        hash_input[offset..offset + i].copy_from_slice(&buf[..i]);
+        offset += i;
+
+        hash_input[offset] = 0x80;
+        
+        let input_length: u64 = (trimmed_input.len() + i) as u64 * 8;
+        hash_input[56..64].copy_from_slice(&input_length.to_le_bytes());
+
+        a = 0x67452301;
+        b = 0xefcdab89;
+        c = 0x98badcfe;
+        d = 0x10325476;
+
+        for chunk in hash_input.chunks(64) {
+            let mut da = a;
+            let mut db = b;
+            let mut dc = c;
+            let mut dd = d;
+
+            let mut m = [0u32; 16];
+            for (i, m_value) in m.iter_mut().enumerate() {
+                let bytes = &chunk[i * 4 .. (i + 1) * 4];
+                *m_value = u32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]);
             }
 
-            f = f.wrapping_add(da).wrapping_add(k[i]).wrapping_add(m[g]);
-            da = dd;
-            dd = dc;
-            dc = db;
-            db = db.wrapping_add(f.rotate_left(s[i]));
+            for i in 0..64 {
+                let mut f: u32;
+                let g: usize;
+
+                if i <= 15 {
+                    f = (db & dc) | (!db & dd);
+                    g = i;
+                } else if i <= 31 {
+                    f = (dd & db) | (!dd & dc); 
+                    g = (5 * i + 1) % 16;
+                } else if i <= 47 {
+                    f = db ^ dc ^ dd;
+                    g = (3 * i + 5) % 16;
+                } else {
+                    f = dc ^ (db | !dd);
+                    g = (7 * i) % 16;
+                }
+
+                f = f.wrapping_add(da).wrapping_add(k[i]).wrapping_add(m[g]);
+                da = dd;
+                dd = dc;
+                dc = db;
+                db = db.wrapping_add(f.rotate_left(s[i]));
+            }
+
+            a = a.wrapping_add(da);
+            b = b.wrapping_add(db);
+            c = c.wrapping_add(dc);
+            d = d.wrapping_add(dd);
         }
 
-        a = a.wrapping_add(da);
-        b = b.wrapping_add(db);
-        c = c.wrapping_add(dc);
-        d = d.wrapping_add(dd);
+        let digest = &a.to_le_bytes();
+        
+        if digest[0] == 0 && digest[1] == 0 && (digest[2] & 0xf0) == 0 && part1 == 0 {
+            part1 = num;
+        }
+
+        if digest[0] == 0 && digest[1] == 0 && digest[2] == 0 && part2 == 0 {
+            part2 = num;
+        }
+
+        if part1 != 0 && part2 != 0 {
+            break;
+        }
+
+        num += 1;
     }
 
-    let mut digest: Vec<u8> = Vec::with_capacity(16);
-    digest.extend(&a.to_le_bytes());
-    digest.extend(&b.to_le_bytes());
-    digest.extend(&c.to_le_bytes());
-    digest.extend(&d.to_le_bytes());
-
-    digest.iter().map(|b| format!("{:02x}", b)).collect::<String>()
+    (part1, part2)
 }
+
 
 #[cfg(test)]
 mod tests {
