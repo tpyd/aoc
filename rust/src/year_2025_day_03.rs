0d05e3f232423d8abf3df93d5d1416054bc2febf
tpyd <112725276+tpyd@users.noreply.github.com>
2025-12-04 21:54:49 +0100

    Day 3 improvements

--- rust/src/year_2025_day_03.rs
+++ rust/src/year_2025_day_03.rs
@@ -29,11 +29,7 @@ pub fn run(input: &str) -> (u64, u64) {
         output_joltage += jolts;
 
         // Part 2
-        let mut joltage = [0u8; 12];
-
-        for i in 0..12 {
-            joltage[i] = bank[i];
-        }
+        let mut bank_joltage = 0;
 
         let mut index = 0;
         for i in 0..12 {
@@ -45,23 +41,10 @@ pub fn run(input: &str) -> (u64, u64) {
                     index = j + 1;
                 }
             }
-            joltage[i] = max;
+            bank_joltage += (max - 48) as u64 * 10u64.pow(11 - i as u32);
         }
 
-        let mut jp2 = 0;
-        for i in 0..12 {
-            jp2 += ((joltage[i]-48) as u64) * 10u64.pow(12 - i as u32) / 10;
-        }
-        output_joltage_part2 += jp2;
-
-        dbg!();
-        let mut jj = [0u8; 12];
-        for i in 0..12 {
-            jj[i] = joltage[i]-48;
-        }
-        let bb: Vec<u8> = bank.iter().map(|x| x - 48).collect();
-        dbg!(&bb, &jj, &jp2);
-
+        output_joltage_part2 += bank_joltage;
     }
 
     (output_joltage, output_joltage_part2)
@@ -72,21 +55,15 @@ mod tests {
     use super::*;
     use crate::utils;
 
-    // #[test]
-    // fn test_part1() {
-    //     let (part1, _) = run("818181911112111");
-    //     assert_eq!(part1, 0);
-    // }    
+    #[test]
+    fn test_part1() {
+        let (part1, _) = run("987654321111111
+811111111111119
+234234234234278
+818181911112111"); 
+        assert_eq!(part1, 357);
+    }    
 
-//     #[test]
-//     fn test_part1() {
-//         let (part1, _) = run("987654321111111
-// 811111111111119
-// 234234234234278
-// 818181911112111"); 
-//         assert_eq!(part1, 357);
-//     }    
-//
     #[test]
     fn test_part2() {
         let (_, part2) = run("987654321111111
@@ -96,13 +73,13 @@ mod tests {
         assert_eq!(part2, 3121910778619);
     }
 
-    // #[test]
-    // fn test_real() {
-    //     let input = utils::read_input(, );
-    //     let (part1, part2) = run(&input);
-    //
-    //     assert_eq!(part1, );
-    //     assert_eq!(part2, );
-    // }
+    #[test]
+    fn test_real() {
+        let input = utils::read_input(2025, 3);
+        let (part1, part2) = run(&input);
+
+        assert_eq!(part1, 17301);
+        assert_eq!(part2, 172162399742349);
+    }
 }
 
