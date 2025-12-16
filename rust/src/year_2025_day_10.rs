b36f05660e7f31c7190acdb914217fca852ce039
tpyd <112725276+tpyd@users.noreply.github.com>
2025-12-16 18:05:19 +0100

    Improved day 10

--- rust/src/year_2025_day_10.rs
+++ rust/src/year_2025_day_10.rs
@@ -1,34 +1,85 @@
 use std::collections::{hash_map::Entry, HashMap};
 
 // TODO change to sum of buttons
-fn combinations(buttons: &[Vec<u32>]) -> Vec<(Vec<usize>, u32)> {
+// TODO does this need to be usize?
+fn combinations(buttons: &[Vec<u32>], len: usize) -> HashMap<u32, Vec<(Vec<u32>, u32)>> {
     let max_len = 2usize.pow(buttons.len() as u32);
-    // TODO change to hashmap
-    let mut paths: Vec<(Vec<usize>, u32)> = Vec::with_capacity(max_len);
+    let mut paths: Vec<(Vec<u32>, u32)> = Vec::with_capacity(max_len);
+    let mut combinations: HashMap<u32, Vec<(Vec<u32>, u32)>> = HashMap::new();
 
-    paths.push((Vec::new(), 0));
+    paths.push((vec![0; len], 0));
 
-    for (i, _) in buttons.iter().enumerate() {
+    for (i, button) in buttons.iter().enumerate() {
         // We either pressed it or not 
         let mut new_paths = paths.clone();
 
-        for (button_indicies, presses) in &paths {
+        for (acc_button, presses) in &paths {
             // Add a path where the button was pushed
-            let mut pushed_indicies = button_indicies.clone();
-            pushed_indicies.push(i);
-            new_paths.push((pushed_indicies, presses + 1));
+            let mut new_acc_button = acc_button.clone();
+            for j in button {
+                new_acc_button[*j as usize] += 1; 
+            }
+
+            // Add both values to hashmap
+            if i == buttons.len() - 1 {
+                let mut parity_pressed = 0u32;
+                for j in 0..new_acc_button.len() {
+                    if new_acc_button[j] % 2 != 0 {
+                        parity_pressed += 1 << j;
+                    }
+                }
+
+                let mut parity_not_pressed = 0u32;
+                for j in 0..acc_button.len() {
+                    if acc_button[j] % 2 != 0 {
+                        parity_not_pressed += 1 << j;
+                    }
+                }
+
+                if (parity_pressed == 0 || parity_not_pressed == 0) && *presses == 3 {
+                    // dbg!("her");
+                    //
+                    // dbg!(&presses, &button, &new_acc_button, &acc_button);
+                }
+
+                // TODO duplicates can occur, maybe filter out, can they??
+                // println!("Adding {parity_pressed:b} with buttons {new_acc_button:?}");
+                match combinations.entry(parity_pressed) {
+                    Entry::Occupied(mut e) => {
+                        let ref mut vec = *e.get_mut();
+                        vec.push((new_acc_button, presses + 1));
+                    },
+                    Entry::Vacant(e) => {
+                        e.insert(vec![(new_acc_button, presses + 1)]);
+                    }
+                }
+
+                match combinations.entry(parity_not_pressed) {
+                    Entry::Occupied(mut e) => {
+                        let ref mut vec = *e.get_mut();
+                        vec.push((acc_button.to_vec(), *presses));
+                    },
+                    Entry::Vacant(e) => {
+                        e.insert(vec![(acc_button.to_vec(), *presses)]);
+                    }
+                }
+            } else {
+                new_paths.push((new_acc_button, presses + 1));
+            }
         }
 
         paths = new_paths;
+
+        // dbg!(&paths);
     }
 
-    paths
+    combinations
 }
 
 fn find(
     joltages: &[u32], 
     buttons: &Vec<Vec<u32>>,
-    button_combinations: &Vec<(Vec<usize>, u32)>,
+    button_combinations: &HashMap<u32, Vec<(Vec<u32>, u32)>>,
     cache: &mut HashMap<Vec<u32>, u32>
 ) -> u32 {
     if joltages.iter().all(|j| *j == 0) {
@@ -37,46 +88,56 @@ fn find(
 
     let mut min_presses = 1000000;
 
-    'outer: for (button_combination, combination_presses) in button_combinations {
+    let mut parity = 0u32;
+    for j in 0..joltages.len() {
+        if joltages[j] % 2 != 0 {
+            parity += 1 << j;
+        }
+    }
+
+    let filtered_buttons = match button_combinations.get(&parity) {
+        Some(b) => b,
+        None => return min_presses
+    };
+
+    'outer: for (acc_button, combination_presses) in filtered_buttons {
         let mut new_joltages = joltages.to_vec();
 
-        for idx in button_combination {
-            let button = &buttons[*idx];
-            for b in button {
-                let joltage_value = new_joltages[*b as usize];
-                if joltage_value == 0 {
-                    continue 'outer;
-                }
-                new_joltages[*b as usize] -= 1;
-            }
-        }
-
-        for i in 0..new_joltages.len() {
-            if new_joltages[i] % 2 != 0 {
+        for (i, v) in acc_button.iter().enumerate() {
+            let value = new_joltages[i];
+            if *v > value {
                 continue 'outer;
             }
+            new_joltages[i] -= v; 
+        }
+        // dbg!(&new_joltages);
+
+        for i in 0..new_joltages.len() {  // TODO merge with above loop
             new_joltages[i] /= 2;
         }
 
-        let mut presses = if let Some(&presses) = cache.get(&new_joltages) {
-            presses
-        } else {
-            let presses = find(&new_joltages, buttons, button_combinations, cache);
-            cache.insert(new_joltages.to_vec(), presses);
-            presses
+        let mut rec_presses = match cache.get(&new_joltages) {
+            Some(&p) => p,
+            None => {
+                let p = find(&new_joltages, buttons, button_combinations, cache);
+                cache.insert(new_joltages.to_vec(), p);
+                p
+            }
         };
+        // let mut rec_presses = find(&new_joltages, buttons, button_combinations, cache);
+        // dbg!(&new_joltages, &rec_presses, &min_presses);
 
-        presses = 2 * presses + combination_presses;
+        rec_presses = 2 * rec_presses + combination_presses;
 
-        if presses < min_presses {  // TODO faster if we swap with .min()?
-            min_presses = presses;
+        if rec_presses < min_presses {  // TODO faster if we swap with .min()?
+            min_presses = rec_presses;
         }
     }
 
     min_presses
 }
 
-pub fn run(input: &str) -> (i32, u32) {
+pub fn run(input: &str) -> (u32, u32) {
     let lines = input
         .trim_end()
         .split("\n");
@@ -89,19 +150,18 @@ pub fn run(input: &str) -> (i32, u32) {
     for (round, line) in lines.enumerate() {
         let (lights_str, buttons_str) = line.split_once(" ").unwrap();
         
-        let num_lights = lights_str.len() - 2;
-        let mut light_goal = Vec::new();
-        for c in lights_str[1..lights_str.len() - 1].chars() {
-            match c {
-                '#' => light_goal.push(true),
-                _ => light_goal.push(false)
-            }
-        }
+        let light_goal = lights_str
+            .chars()
+            .filter(|v| *v == '#' || *v == '.')
+            .enumerate()
+            .filter(|(_, v)| *v == '#')
+            .fold(0, |acc, (i, _)| acc + (1 << i));
 
         let button_parts: Vec<&str> = buttons_str
             .split_whitespace()
             .collect();
 
+        // TODO maybe parse as usize
         let mut buttons: Vec<Vec<u32>> = button_parts
             .clone()
             .into_iter()
@@ -124,58 +184,31 @@ pub fn run(input: &str) -> (i32, u32) {
             .map(|n| n.parse::<usize>().unwrap())
             .collect();
 
-        // dbg!(&joltage_requirements);
-
         let mut lights = Vec::new();
-        for _ in 0..num_lights {
+        for _ in 0..lights_str.len() - 2 {
             lights.push(false);
         }
 
-        // dbg!(&lights, &button_list, &light_goal);
+        let len = joltage_goal.len();
+        let button_combinations = combinations(&buttons, len); 
 
-        let mut possibilities = vec![(lights, vec![])];
-        
-        let mut button_presses = 0;
-        'outer: loop {
-            let possibilities_copy = possibilities.clone();
-            possibilities.clear();
-
-            button_presses += 1;
-
-            for (current_lights, button_sequence) in possibilities_copy {
-                for button in &buttons {
-                    let mut new_lights = current_lights.clone();
-                    let mut new_buttons = button_sequence.clone();
-
-                    for b in button {
-                        new_lights[*b as usize] ^= true;
-                    }
-
-                    if new_lights == light_goal {
-                        part1 += button_presses;
-                        break 'outer;
-                    }
-
-                    new_buttons.push(button);
-                    possibilities.push((new_lights, new_buttons)); 
-                }
-            }
-        }
+        // Part 1
+        part1 += button_combinations
+            .get(&light_goal)
+            .unwrap()
+            .iter()
+            .min_by(|a, b| a.1.cmp(&b.1))
+            .unwrap()
+            .1;
 
         // Part 2
         let mut data = Vec::new();
-        for i in joltage_goal {
-            data.push(i as u32);
+        for i in &joltage_goal {
+            data.push(*i as u32);
         }
 
-        let button_combinations = combinations(&buttons);
-        let mut cache = HashMap::new();
-
-        let button_presses = find(&data, &buttons, &button_combinations, &mut cache);
+        let button_presses = find(&data, &buttons, &button_combinations, &mut HashMap::new());
         part2 += button_presses;
-
-        // let iterstr = format!("Iteration {} / {}", round+1, length);
-        // dbg!(iterstr);
     }
 
     (part1, part2)
@@ -185,6 +218,13 @@ pub fn run(input: &str) -> (i32, u32) {
 mod tests {
     use super::*;
     use crate::utils;
+
+    #[test]
+    fn test_single() {
+        let (part1, part2) = run("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}");
+        assert_eq!(part1, 2);
+        assert_eq!(part2, 10);
+    }
 
     #[test]
     fn test_part1() {
