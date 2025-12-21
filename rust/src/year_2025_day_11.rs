f6963fad34519556ca5164d597e7385b1ac8188c
tpyd <112725276+tpyd@users.noreply.github.com>
2025-12-21 10:46:53 +0100

    Improved day 11

--- rust/src/year_2025_day_11.rs
+++ rust/src/year_2025_day_11.rs
@@ -1,13 +1,43 @@
-use core::panic;
-use std::collections::{hash_map::Entry, HashMap, VecDeque};
+use std::collections::{HashMap, VecDeque};
+
+fn count_paths(
+    graph: &HashMap<String, Vec<String>>, 
+    from: String, 
+    to: String, 
+    topo_order: &Vec<String>
+) -> u128 {
+    let mut dp: HashMap<String, u128> = graph
+        .keys()
+        .map(|k| (k.to_string(), 0))
+        .collect();
+
+    dp.insert(from, 1);
+    dp.insert(to.clone(), 0);  // Neccessary for part 1 test since it doesnt have fft or dac
+                               
+    for u in topo_order {
+        let u_val = dp.get(u).unwrap().clone();
+
+        if u_val == 0 {
+            continue;
+        }
+
+        for v in graph.get(u).unwrap() {
+            dp.entry(v.to_string())
+                .and_modify(|e| *e = *e + u_val);
+        }
+    }
+
+    *dp.get(&to).unwrap()
+}
 
 pub fn run(input: &str) -> (u128, u128) {
     let lines = input
         .trim_end()
         .split("\n");
     
-    // TODO this can be &str i think?
+    // TODO this (everything) can be &str i think?
     let mut graph: HashMap<String, Vec<String>> = HashMap::new();
+    let mut incoming: HashMap<String, u32> = HashMap::new();
 
     for line in lines {
         let parts: Vec<String> = line
@@ -18,112 +48,59 @@ pub fn run(input: &str) -> (u128, u128) {
         let from = parts[0].to_owned();
         let to = &parts[1..];
 
-        graph.insert(from, to.to_vec());
+        graph.insert(from.to_string(), to.to_vec());
+        incoming.insert(from, 0);
     }
 
     graph.insert("out".to_string(), Vec::new());
-    
-    fn topological_sort(graph: &HashMap<String, Vec<String>>) -> Vec<String> {
-        let mut indeg: HashMap<String, u32> = HashMap::new();
-        
-        for node in graph.keys() {
-            indeg.insert(node.to_string(), 0);
+
+    for u in graph.keys() {
+        for v in graph.get(u).unwrap() {
+            incoming
+                .entry(v.to_string())
+                .and_modify(|e| *e = *e + 1)
+                .or_insert(1);
         }
-
-        for u in graph.keys() {
-            for v in graph.get(u).unwrap() {
-                match indeg.entry(v.to_string()) {
-                    Entry::Occupied(mut e) => {
-                        e.insert(e.get() + 1);
-                    },
-                    Entry::Vacant(e) => {
-                        e.insert(1);
-                    }
-                }
-            }
-        }
-
-        let mut q: VecDeque<String> = VecDeque::new();
-        let no_in = indeg.iter()
-            .filter(|(_, v)| **v == 0)
-            .map(|(k, _)| k);
-        for k in no_in {
-            q.push_back(k.to_string());
-        }
-
-        let mut order: Vec<String> = Vec::new();
-
-        while let Some(u) = q.pop_front() {
-            order.push(u.clone());
-
-            if graph.get(&u).is_none() {
-                continue;
-            }
-
-            for v in graph.get(&u).unwrap() {
-                match indeg.entry(v.to_string()) {
-                    Entry::Occupied(mut e) => {
-                        let new_val = e.get() - 1;
-                        e.insert(new_val);
-                        if new_val == 0 {
-                            q.push_back(v.to_string());
-                        }
-                    },
-                    Entry::Vacant(_) => panic!(),
-                }
-            }
-        }
-
-        return order;
     }
 
-    fn count_paths(
-        graph: &HashMap<String, Vec<String>>, 
-        from: String, 
-        to: String, 
-        topo_order: &Vec<String>
-    ) -> u128 {
-        let mut dp: HashMap<String, u128> = HashMap::new();
-        
-        for device in graph.keys() {
-            dp.insert(device.to_string(), 0);
+    let mut queue: VecDeque<String> = incoming.iter()
+        .filter(|(_, v)| **v == 0)
+        .map(|(k, _)| k.to_string())
+        .collect();
+
+    let mut sorted: Vec<String> = Vec::new();
+
+    while let Some(u) = queue.pop_front() {
+        sorted.push(u.clone());
+
+        let dest = graph.get(&u);
+        if dest.is_none() {
+            continue;
         }
 
-        dp.insert(from, 1);
-        dp.insert(to.clone(), 0);  // Neccessary for part 1 test since it doesnt have fft or dac
-                                   
-        for u in topo_order {
-            if *dp.get(u).unwrap() == 0 {
-                continue;
-            }
-            let u_val = dp.get(u).unwrap().clone();
-
-            for v in graph.get(u).unwrap() {
-                match dp.entry(v.to_string()) {
-                    Entry::Occupied(mut e) => {
-                        e.insert(e.get() + u_val);
+        for v in dest.unwrap() {
+            incoming
+                .entry(v.to_string())
+                .and_modify(|e| {
+                    *e = *e - 1;
+                    if *e == 0 {
+                        queue.push_back(v.to_string()); 
                     }
-                    Entry::Vacant(_) => panic!("missing out value in dp")
-                }
-            }
+                });
         }
-
-        *dp.get(&to).unwrap()
     }
 
     // TODO check if all explicit types are needed
     // TODO make owned string into references where possible
     // TODO change variable names to be more in line with the task
     // TODO lower u128 if possible
-    // TODO can we filter topo_order before these calls?
+    // TODO can we filter `sorted` before these calls to make it iterate less?
     // TODO add possibility of dac before fft
-    let topo_order = topological_sort(&graph);
-    
-    let part1 = count_paths(&graph, "you".to_string(), "out".to_string(), &topo_order);
+    let part1 = count_paths(&graph, "you".to_string(), "out".to_string(), &sorted);
 
-    let a = count_paths(&graph, "svr".to_string(), "fft".to_string(), &topo_order);
-    let b = count_paths(&graph, "fft".to_string(), "dac".to_string(), &topo_order);
-    let c = count_paths(&graph, "dac".to_string(), "out".to_string(), &topo_order);
+    let a = count_paths(&graph, "svr".to_string(), "fft".to_string(), &sorted);
+    let b = count_paths(&graph, "fft".to_string(), "dac".to_string(), &sorted);
+    let c = count_paths(&graph, "dac".to_string(), "out".to_string(), &sorted);
     let part2 = a * b * c;
 
     (part1, part2)
