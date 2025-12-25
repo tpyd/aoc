918b641f29eb1faf4eede478527e0e6274ec5def
tpyd <112725276+tpyd@users.noreply.github.com>
2025-12-25 09:33:02 +0100

    Improved day 4

--- src/year_2025_day_04.rs
+++ src/year_2025_day_04.rs
@@ -1,209 +1,117 @@
 pub fn run(input: &str) -> (usize, usize) {
     let mut rolls = input
-        .trim_end()
-        .split('\n')
+        .lines()
         .map(|x| x.chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
 
-    let movable = get_movable(&rolls);
-    let part1 = movable.len();
+    let mut movable = Vec::new();
+    let mut part1 = 0;
+    let mut part2 = 0;
 
-    let mut total_removed = 0;
     loop {
-        let moved = get_movable(&rolls);
+        movable.clear();
+        get_movable(&rolls, &mut movable);
 
-        if moved.len() == 0 {
+        if movable.len() == 0 {
             break;
         }
 
-        total_removed += moved.len();
+        if part1 == 0 {
+            part1 = movable.len();
+        }
 
-        for m in moved {
-            rolls[m.1][m.0] = '.';
+        part2 += movable.len();
+
+        for (x, y) in &movable {
+            rolls[*y][*x] = '.';
         }
     }
 
-    (part1, total_removed)
+    (part1, part2)
 }
 
-fn get_movable(rolls: &Vec<Vec<char>>) -> Vec<(usize, usize)> {
-    let width = rolls[0].len();
+fn get_movable(rolls: &[Vec<char>], movable: &mut Vec<(usize, usize)>) {
     let height = rolls.len();
+    let width = rolls[0].len();
 
-    let mut movable = Vec::new();
-
-    // Center
-    for y in 1..height-1 {
-        for x in 1..width-1 {
-            let cell = rolls[y][x];
-
-            if cell != '@' {
+    for y in 0..height {
+        for x in 0..width {
+            if rolls[y][x] != '@' {
                 continue;
             }
 
-            let top = &rolls[y-1][x-1..=x+1];
-            let bottom = &rolls[y+1][x-1..=x+1];
-            let left = &rolls[y][x-1];
-            let right = &rolls[y][x+1];
+            let mut adjacent: u8 = 0;
 
-            let mut counter = 0;
-            counter += top.iter().filter(|x| **x == '@').count();
-            counter += bottom.iter().filter(|x| **x == '@').count();
-            counter += (*left == '@') as usize;
-            counter += (*right == '@') as usize;
-            
-            if counter < 4 {
+            if y > 0 {
+                // Top
+                let cell = rolls[y - 1][x];  
+                if cell == '@' {
+                    adjacent += 1;
+                }
+
+                // Top-left
+                if x > 0 {
+                    let cell = rolls[y - 1][x - 1];  
+                    if cell == '@' {
+                        adjacent += 1;
+                    }
+                }
+
+                // Top-right
+                if x < width - 1 {
+                    let cell = rolls[y - 1][x + 1];  
+                    if cell == '@' {
+                        adjacent += 1;
+                    }
+                }
+            }
+
+            // Left
+            if x > 0 {
+                let cell = rolls[y][x - 1];  
+                if cell == '@' {
+                    adjacent += 1;
+                }
+            }
+
+            // Right
+            if x < width - 1 {
+                let cell = rolls[y][x + 1];  
+                if cell == '@' {
+                    adjacent += 1;
+                }
+            }
+
+            if y < height - 1 {
+                // Bottom
+                let cell = rolls[y + 1][x];
+                if cell == '@' {
+                    adjacent += 1;
+                }
+
+                // Bottom-left
+                if x > 0 {
+                    let cell = rolls[y + 1][x - 1];  
+                    if cell == '@' {
+                        adjacent += 1;
+                    }
+                }
+
+
+                // Bottom-right
+                if x < width - 1 {
+                    let cell = rolls[y + 1][x + 1];  
+                    if cell == '@' {
+                        adjacent += 1;
+                    }
+                }
+            }
+
+            if adjacent < 4 {
                 movable.push((x, y));
             }
         }
     }
-
-    // top row
-    for x in 1..=width-2 {
-        let cell = rolls[0][x];
-
-        if cell != '@' {
-            continue;
-        }
-
-        let bottom = &rolls[1][x-1..=x+1];
-        let left = &rolls[0][x-1];
-        let right = &rolls[0][x+1];
-
-        let mut counter = 0;
-        counter += bottom.iter().filter(|x| **x == '@').count();
-        counter += (*left == '@') as usize;
-        counter += (*right == '@') as usize;
-        
-        if counter < 4 {
-            movable.push((x, 0));
-        }
-    }
-
-    // bottom row
-    for x in 1..width-1 {
-        let cell = rolls[height-1][x];
-
-        if cell != '@' {
-            continue;
-        }
-
-        let top = &rolls[height-2][x-1..=x+1];
-        let left = &rolls[height-1][x-1];
-        let right = &rolls[height-1][x+1];
-
-        let mut counter = 0;
-        counter += top.iter().filter(|x| **x == '@').count();
-        counter += (*left == '@') as usize;
-        counter += (*right == '@') as usize;
-        
-        if counter < 4 {
-            movable.push((x, height-1));
-        }
-    }
-
-    // left row
-    for y in 1..height-1 {
-        let cell = rolls[y][0];
-
-        if cell != '@' {
-            continue;
-        }
-
-        let top = &rolls[y-1][0..=1];
-        let bottom = &rolls[y+1][0..=1];
-        let right = &rolls[y][1];
-
-        let mut counter = 0;
-        counter += top.iter().filter(|x| **x == '@').count();
-        counter += bottom.iter().filter(|x| **x == '@').count();
-        counter += (*right == '@') as usize;
-        
-        if counter < 4 {
-            movable.push((0, y));
-        }
-    }
-
-    // right row
-    for y in 1..height-1 {
-        let cell = rolls[y][width-1];
-
-        if cell != '@' {
-            continue;
-        }
-
-        let top = &rolls[y-1][width-2..=width-1];
-        let bottom = &rolls[y+1][width-2..=width-1];
-        let left = &rolls[y][width-2];
-
-        let mut counter = 0;
-        counter += top.iter().filter(|x| **x == '@').count();
-        counter += bottom.iter().filter(|x| **x == '@').count();
-        counter += (*left == '@') as usize;
-        
-        if counter < 4 {
-            movable.push((width-1, y));
-        }
-    }
-
-    // Corners
-    if rolls[0][0] == '@' {
-        let mut counter = 0;
-
-        let bottom = &rolls[1][0..=1];
-        let right = &rolls[0][1];
-        
-        counter += bottom.iter().filter(|x| **x == '@').count();
-        counter += (*right == '@') as usize;
-
-        if counter < 4 {
-            movable.push((0, 0));
-        }
-    }
-
-    if rolls[0][width-1] == '@' {
-        let mut counter = 0;
-
-        let bottom = &rolls[1][width-2..=width-1];
-        let left = &rolls[0][width-2];
-        
-        counter += bottom.iter().filter(|x| **x == '@').count();
-        counter += (*left == '@') as usize;
-
-        if counter < 4 {
-            movable.push((width-1, 0));
-        }
-    }
-
-    if rolls[height-1][0] == '@' {
-        let mut counter = 0;
-
-        let top = &rolls[height-2][0..=1];
-        let right = &rolls[height-1][1];
-        
-        counter += top.iter().filter(|x| **x == '@').count();
-        counter += (*right == '@') as usize;
-
-        if counter < 4 {
-            movable.push((0, height-1));
-        }
-    }
-
-    if rolls[height-1][width-1] == '@' {
-        let mut counter = 0;
-
-        let top = &rolls[height-1][width-2..=width-1];
-        let left = &rolls[height-1][width-2];
-        
-        counter += top.iter().filter(|x| **x == '@').count();
-        counter += (*left == '@') as usize;
-
-        if counter < 4 {
-            movable.push((width-1, height-1));
-        }
-    }
-
-    movable
 }
 
 #[cfg(test)]
